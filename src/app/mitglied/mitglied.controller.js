effc6b5b31f27ad570f3a1b326073c3033a364a8
Marcel Arnold (U116399) <marcel.arnold@mobi.ch>
2015-11-20 15:36:31 +0100

    remove unused parameter key

--- src/app/mitglied/mitglied.controller.js
+++ src/app/mitglied/mitglied.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -8,22 +8,22 @@
       vm.instr = $routeParams.instr;
 
       dataFactory.getMitglied(vm.instr).then(function (response) {
-          vm.entries = response.data;
-          angular.forEach(vm.entries, function(value, key) {
-            value.captionArray = [
-              value.vorname + " " + value.nachname,
-              value.spitzname,
-              "Eintritt: " + value.eintritt
-            ];
-          });
-        }, function () {
+        vm.entries = response.data;
+        angular.forEach(vm.entries, function (value) {
+          value.captionArray = [
+            value.vorname + " " + value.nachname,
+            value.spitzname,
+            "Eintritt: " + value.eintritt
+          ];
         });
+      }, function () {
+      });
 
       vm.openLightboxModal = function (index) {
         Lightbox.openModal(vm.entries, index);
       };
 
-  }]);
+    }]);
 
 
 })();

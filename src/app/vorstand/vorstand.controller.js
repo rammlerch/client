b964813145854157fb9c0db54383e6f412ec0e17
Marcel Arnold <mail@maesi.org>
2015-11-20 15:37:32 +0100

    show address and phone number (resolved #1)

--- src/app/vorstand/vorstand.controller.js
+++ src/app/vorstand/vorstand.controller.js
@@ -8,6 +8,17 @@
 
       dataFactory.getVorstand().then(function (response) {
         vm.entries = response.data;
+        angular.forEach(vm.entries, function (value) {
+          var newlines = 0;
+          if (value.adresse == null) {
+            newlines += 2;
+          }
+          if (value.telefon == null) {
+            newlines += 1;
+          }
+          value.newlines = new Array(newlines);
+          value.newlineshtml = "<br />&nbsp;<br />&nbsp;<br />&nbsp;";
+        });
       }, function () {
       });
 

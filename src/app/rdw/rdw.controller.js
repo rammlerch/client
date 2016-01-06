f565e7e34f4ae6c6c46137bac8d35010c6614e2d
U116399 <marcel.arnold@mobi.ch>
2016-01-06 18:36:42 +0100

    button for switching from form to result

--- src/app/rdw/rdw.controller.js
+++ src/app/rdw/rdw.controller.js
@@ -5,9 +5,11 @@
     .module('rammler')
     .controller('RdwController', function (dataFactory) {
       var vm = this;
+      vm.showForm = false;
       vm.vote = 0;
       dataFactory.getRammlerDerWoche().then(function (response) {
         vm.data = response.data;
+        vm.showForm = vm.data.canVote;
       });
 
       vm.doVote = function() {
@@ -20,6 +22,12 @@
             }
           }
           });
+      }
+
+      vm.toggleForm = function() {
+        if(vm.data.canVote) {
+          vm.showForm = !vm.showForm;
+        }
       }
 
       vm.inPerc = function(stimme) {
